e94c98bfb61b571f1e25b67e44e50e3d324da041
sunghyun <20190171@inu.ac.kr>
2024-10-29 15:52:49 +0900

    chore : 코틀린 프로젝트 세팅

---
+++ src/main/kotlin/com/grepp/somun/SomunApplication.kt
@@ -0,0 +1,11 @@
+package com.grepp.somun
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SomunApplication
+
+fun main(args: Array<String>) {
+    runApplication<SomunApplication>(*args)
+}
---
+++ src/test/kotlin/com/grepp/somun/SomunApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.grepp.somun
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SomunApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
