e021aa98fbb065f5f0a27fde72aaa739f65e641c
sunghyun <20190171@inu.ac.kr>
2024-10-30 17:26:06 +0900

    feat : BaseEntity 구현

--- src/main/kotlin/com/grepp/somun/SomunApplication.kt
+++ src/main/kotlin/com/grepp/somun/SomunApplication.kt
@@ -2,6 +2,7 @@ package com.grepp.somun
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 class SomunApplication
