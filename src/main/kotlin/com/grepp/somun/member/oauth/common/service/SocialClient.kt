57181092830db6611771652104373c6a601b4f1f
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-11-04 09:18:56 +0900

    feat: SocialClient 인터페이스 구현

---
+++ src/main/kotlin/com/grepp/somun/member/oauth/common/service/SocialClient.kt
@@ -0,0 +1,16 @@
+package com.grepp.somun.member.oauth.common.service
+
+import com.fasterxml.jackson.core.JsonProcessingException
+import com.grepp.somun.member.oauth.common.dto.SocialMemberCheckDto
+
+interface SocialClient {
+    @Throws(JsonProcessingException::class)
+    fun getAccessToken(code: String): String {
+        throw UnsupportedOperationException("해당 provider에서는 제공 불가")
+    }
+
+    fun getAccessToken(code: String, state: String): String
+
+    @Throws(JsonProcessingException::class)
+    fun getMemberInfo(accessToken: String): SocialMemberCheckDto
+}
